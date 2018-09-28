c7e3dec320f78059ae1a1d122dfded8fc2788967
wuyuwu <839427538@qq.com>
2018-09-28 15:15:55 +0800

    参考spring boot官方给出的例子实现

---
+++ src/main/java/com/neo4j/start/neo4j/service/OperationService.java
@@ -0,0 +1,29 @@
+package com.neo4j.start.neo4j.service;
+
+
+/**
+ * 对neo4j的增删改查
+ *
+ */
+public class OperationService {
+
+
+    public void add(){
+
+    }
+
+
+    public void del(){
+
+    }
+
+
+    public void update(){
+
+    }
+
+
+    public void search(){
+
+    }
+}
